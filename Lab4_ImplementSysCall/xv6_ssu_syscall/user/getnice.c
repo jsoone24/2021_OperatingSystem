f125b121849cfde5906928e529bf18dc2e05ab00
Jongsoo <jsoone24@gmail.com>
2021-04-05 18:06:07 +0900

    code update

--- Lab4_ImplementSysCall/xv6_ssu_syscall/user/getnice.c
+++ Lab4_ImplementSysCall/xv6_ssu_syscall/user/getnice.c
@@ -4,11 +4,24 @@
 
 int main(int argc, char **argv)
 {
+    int result = 0;
+    int pid = 0;
+    
   if (argc < 2)
   {
     printf(2, "usage: getnice pid ...\n");
     exit();
   }
-    getnice(atoi(argv[1]));
+pid = atoi(argv[1]);
+  result = getnice(atoi(argv[1])); 
+
+  if(result < 0)
+  {
+      printf(2, "non-existing process\n");
+  }
+  else
+  {
+      printf(2, "pid : %d nice : %d\n", pid, result);
+  }
   exit();
 }
